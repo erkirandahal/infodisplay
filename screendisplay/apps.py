ac120454673da4aebff6fa8cbfadf3a3cdb82d6c
unknown <erkirandahal@gmail.com>
2020-12-20 13:00:03 +0545

    Application created, screen app started along with users modal

---
+++ screendisplay/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ScreendisplayConfig(AppConfig):
+    name = 'screendisplay'
---
+++ users/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from django.contrib.auth.models import User, Group
+from django.contrib.auth.forms import UserCreationForm
+from .models import Profile
+
+class UserRegisterForm(UserCreationForm):
+    email = forms.EmailField()
+    # alternate_email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email', 'password1', 'password2']
+        help_texts = {
+            'username': None,
+            'email': None,
+            'password1': None,
+            'password2': None,
+            'password': None,
+        }
+
+class UserUpdateForm(forms.ModelForm):
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
+class ProfileUpdateForm(forms.ModelForm):
+    class Meta:
+        model = Profile
+        fields = ['image']
